a10782ce0787618931a73cceba0d608ce5e0d17f
Lauren Swersky <lauren.swersky@gmail.com>
2022-10-18 11:37:06 -0400

    Add Rock-Paper-Scissors.js

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,6 +1,34 @@
+// computer returns a random play
 function getComputerChoice() {
     let options = ["rock", 'paper', 'scissor'];
     let randomChoice = options[Math.floor(Math.random()*options.length)];
-    return randomChoice;
-    }
+    return randomChoice.toLowerCase();
+    }   
+
+// user inputs selection (playerSelection)
+function playerSelection() {
+    let playerSelection = prompt('Please choose rock, paper or scissor.').toLowerCase();
+     return playerSelection;
+     }
+
+// computer returns their selection (computerSelection)
+// if values are equal, return 'tie!'
+// if playerSelection > computerSelection, return 'win' msg
+// if computerSelection > playerSelection, return 'lose' msg
+function singleRound(playerSelection, compSelection) {
+    function getComputerChoice()
+    function playerSelection()
+    let compSelection = getComputerChoice()
+    if (playerSelection === compSelection) {
+        return 'Tie!';
+    } else if ((playerSelection === 'rock' && compSelection === 'scissor') ||
+                (playerSelection === 'paper' && compSelection === 'rock') || 
+                (playerSelection === 'scissor' && compSelection === 'paper')) {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }    
+}
+
+
 
