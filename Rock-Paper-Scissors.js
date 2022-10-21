7d7b7167cd142cd4009791761181410ff3c4cd7c
Lauren Swersky <lauren.swersky@gmail.com>
2022-10-21 15:25:10 -0400

    Rock_Paper-Scissors.html

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,34 +1,49 @@
+     let playerScore = 0
+     let compScore = 0
+
 // computer returns a random play
-function getComputerChoice() {
+function compSelection() {
     let options = ["rock", 'paper', 'scissor'];
     let randomChoice = options[Math.floor(Math.random()*options.length)];
     return randomChoice.toLowerCase();
     }   
 
-// user inputs selection (playerSelection)
+// user inputs selection 
 function playerSelection() {
     let playerSelection = prompt('Please choose rock, paper or scissor.').toLowerCase();
      return playerSelection;
      }
 
-// computer returns their selection (computerSelection)
-// if values are equal, return 'tie!'
-// if playerSelection > computerSelection, return 'win' msg
-// if computerSelection > playerSelection, return 'lose' msg
+// play single round
 function singleRound(playerSelection, compSelection) {
-    function getComputerChoice()
-    function playerSelection()
-    let compSelection = getComputerChoice()
+    console.log(compSelection);
+    console.log(playerSelection);
     if (playerSelection === compSelection) {
-        return 'Tie!';
+        console.log('Tie! Play again!');
     } else if ((playerSelection === 'rock' && compSelection === 'scissor') ||
                 (playerSelection === 'paper' && compSelection === 'rock') || 
                 (playerSelection === 'scissor' && compSelection === 'paper')) {
-        return 'You win!';
+        playerScore++
+        console.log(playerScore)
+        console.log('You\'ve won this round! Play again!');
     } else {
-        return 'You lose!';
-    }    
+        compScore++
+        console.log(compScore)
+        console.log('Sorry, you\'ve lost this round! Play again!');
+    }   
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        singleRound(playerSelection(), compSelection());
+    }
+    if (playerScore > compScore) {
+        console.log('Congrats! You won the game.')
+    }
+    else if (playerScore < compScore) {
+        console.log('You lost the game, sorry!')
+    }
+}
 
+game()
 
